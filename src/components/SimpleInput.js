76f5d7012f6c9f5fe69451ba5d3bc4ca7bcebed8
rhobbikhaidir <rhobbie09@gmail.com>
2022-05-21 15:04:28 +0700

    validation using hooks

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,58 @@
+import React, { useState, useRef } from "react";
+
+const SimpleInput = (props) => {
+  const nameInput = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameValid, setEnteredNameValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const changeNameHandler = (event) => {
+    // console.log(event.target.value);
+
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameValid(false);
+      return;
+    }
+    setEnteredNameValid(true);
+    console.log(enteredName);
+    const valueNameRef = nameInput.current.value;
+
+    console.log(valueNameRef);
+  };
+
+  const nameInputIsInvalid = !enteredNameValid && enteredNameTouched;
+
+  const nameInputClass = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClass}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInput}
+          onChange={changeNameHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not Empty</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
