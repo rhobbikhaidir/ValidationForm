744e6f796f17d95558170623976da1a6d0cc144e
rhobbikhaidir <rhobbie09@gmail.com>
2022-05-23 12:59:34 +0700

    create another form validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,18 +1,52 @@
 import React, { useState } from "react";
 
-const BasicForm = (props) => {
+const BasicForm = () => {
   const [firstName, setFirstName] = useState("");
   const [touchedFirstName, setTouchedFirstName] = useState(false);
 
-  const firstNameIsValid = firstName.trim() !== "";
+  const [lastName, setLastName] = useState("");
+  const [touchedLastName, setTouchedLastName] = useState(false);
 
+  const [email, setEmail] = useState("");
+  const [touchedEmail, setTouchedEmail] = useState(false);
+
+  const firstNameIsValid = firstName.trim() !== "";
   const firstNameHasError = !firstNameIsValid && touchedFirstName;
+
+  const lastNameIsValid = lastName.trim() !== "";
+  const lastNameHasError = !lastNameIsValid && touchedLastName;
+
+  const emailIsValid = email.includes("@");
+  const emailHasError = !emailIsValid && touchedEmail;
+
+  const formIsValid = false;
+
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
 
   const firstNameChangeHandler = (event) => {
     setFirstName(event.target.value);
   };
+
+  const lastNameChangeHandler = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
   const firstNameBlurHandler = () => {
     setTouchedFirstName(true);
+  };
+
+  const lastNameBlurHandler = () => {
+    setTouchedLastName(true);
+  };
+
+  const emailBlurHandler = () => {
+    setTouchedEmail(true);
   };
 
   const handleSubmit = (e) => {
@@ -21,6 +55,15 @@ const BasicForm = (props) => {
   const firstNameInputClass = firstNameHasError
     ? "form-control invalid"
     : "form-control";
+
+  const lastNameInputClass = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClass = emailHasError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="control-group">
@@ -37,17 +80,35 @@ const BasicForm = (props) => {
             <p className="error-text">First Name must be a empty</p>
           )}
         </div>
-        <div className="form-control">
+        <div className={lastNameInputClass}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+            value={lastName}
+          />
+          {lastNameHasError && (
+            <p className="error-text">Last Name must be a empty</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={emailInputClass}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={email}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p className="error-text">Please email Correct</p>}
       </div>
       <div className="form-actions">
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={!formIsValid}>
+          Submit
+        </button>
       </div>
     </form>
   );
